5deab0b9edf51e24cb70d9d1773cd42420084009
Franco Gatti <francogatti.sdp@outlook.com>
2025-02-26 23:03:51 -0300

    style footer

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
 import { hashSync, compareSync, genSaltSync } from "bcrypt";
 
-export const hash = (password) => hashSync(password, genSaltSync(process.env.SALT))
+export const hash = (password) => hashSync(password, genSaltSync(parseInt(process.env.SALT)))
 export const PassValidation = (passSent, passDB) => compareSync(passSent, passDB)
