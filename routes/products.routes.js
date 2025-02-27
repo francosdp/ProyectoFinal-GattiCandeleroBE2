32626679a91c2535a11e1cbdf6a84ab38861b7f1
Franco Gatti <francogatti.sdp@outlook.com>
2025-02-27 17:55:57 -0300

    Solucionando conflictos y middleware rol Usuario/Admin

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getProduct,getProducts,updateProduct,deleteProduct,createProduct } from "../controllers/productsManager.js";
+import { authorization } from "../config/middlewares.js";
 
 
 const productRouter=Router()
 
 productRouter.get('/', getProducts)
 productRouter.get('/:pid', getProduct)
-productRouter.post('/', createProduct)
-productRouter.put('/:pid', updateProduct)
-productRouter.delete('/:pid', deleteProduct)
+productRouter.post('/', authorization("Admin"),createProduct)
+productRouter.put('/:pid',authorization("Admin"), updateProduct)
+productRouter.delete('/:pid', authorization("Admin"),deleteProduct)
 
 export default productRouter
